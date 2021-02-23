d396239ef52dc5ec03346ac0dec6876f99e74208
Gabriel Ballester <gaby.ballester@gmail.com>
2021-02-23 17:16:24 +0100

    Refresco de accessToken correcto

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,26 +4,54 @@ import jwtDecode from "jwt-decode";
 
 export function getAccessTokenApi() {
   const accessToken = localStorage.getItem(ACCESS_TOKEN);
-
   //si access token false o null retorna null
-  if (!accessToken || accessToken === "null") {
-    return null;
-  }
-
+  if (!accessToken || accessToken === "null") { return null; }
   // si ha expirado devuelvo null, en caso contario devuelvo accessToken
   return expirationCheck(accessToken) ? null : accessToken;
-
-  return accessToken;
 }
 
 export function getRefreshTokenApi() {
   const refreshToken = localStorage.getItem(REFRESH_TOKEN);
   // si refreshToken es false o null retorno null
-  if (!refreshToken || refreshToken === "null") {
-    return null;
-  }
+  if (!refreshToken || refreshToken === "null") { return null; }
   // si ha caducado devuelve null, de lo contrario, refreshToken
   return expirationCheck(refreshToken) ? null : refreshToken;
+}
+
+export function refreshAccessTokenApi(refreshToken) {
+  const url = `${basePath}/${apiVersion}/refresh-access-token`;
+  const bodyObj = {
+    refreshToken: refreshToken
+  };
+  const params = {
+    method: "POST",
+    body: JSON.stringify(bodyObj),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  fetch(url, params)
+    .then(response => {
+      if (response.status !== 200) {
+        return null;
+      }
+      return response.json();
+    })
+    .then(result => {
+      if (!result) {
+        logout();
+      } else {
+        const { accessToken, refreshToken } = result;
+        localStorage.setItem(ACCESS_TOKEN, accessToken);
+        localStorage.setItem(REFRESH_TOKEN, refreshToken);
+      }
+    });
+}
+
+export function logout() {
+  localStorage.removeItem(ACCESS_TOKEN);
+  localStorage.removeItem(REFRESH_TOKEN);
 }
 
 // devuelve true si ha expirado y false si no
@@ -34,43 +62,4 @@ function expirationCheck(token) {
   const now = (Date.now() + seconds) / 1000;
   return now > exp;
 }
-
-
-
-// export function refreshAccessTokenApi(refreshToken) {
-//   const url = `${basePath}/${apiVersion}/refresh-access-token`;
-//   const bodyObj = {
-//     refreshToken: refreshToken
-//   };
-//   const params = {
-//     method: "POST",
-//     body: JSON.stringify(bodyObj),
-//     headers: {
-//       "Content-Type": "application/json"
-//     }
-//   };
-
-//   fetch(url, params)
-//     .then(response => {
-//       if (response.status !== 200) {
-//         return null;
-//       }
-//       return response.json();
-//     })
-//     .then(result => {
-//       if (!result) {
-//         logout();
-//       } else {
-//         const { accessToken, refreshToken } = result;
-//         localStorage.setItem(ACCESS_TOKEN, accessToken);
-//         localStorage.setItem(REFRESH_TOKEN, refreshToken);
-//       }
-//     });
-// }
-
-// export function logout() {
-//   localStorage.removeItem(ACCESS_TOKEN);
-//   localStorage.removeItem(REFRESH_TOKEN);
-// }
-
 
