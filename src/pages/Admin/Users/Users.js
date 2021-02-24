e81a545371365658613f9229552c4c86bf48efc1
Gabriel Ballester <gaby.ballester@gmail.com>
2021-02-24 13:17:51 +0100

    Consiguiendo usuarios activos / inactivos

--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import { getAccessTokenApi } from "../../../api/auth";
+import { getUsersActiveApi } from "../../../api/user";
+
 import "./Users.scss";
 
 export default function Users() {
+  const [usersActive, setUsersActive] = useState([])
+  const [usersInactive, setUsersInactive] = useState([])
+  const token = getAccessTokenApi();
 
+  useEffect(() => {
+    getUsersActiveApi(token, true).then(response => {
+      setUsersActive(response);
+    })
+    getUsersActiveApi(token, false).then(response => {
+      setUsersInactive(response);
+      
+    })
+  }, [token]);
 
   return (
     <div>
