f15518cd1d47475db3e496f6cf3ee4a6454a41ef
Gabriel Ballester <gaby.ballester@gmail.com>
2021-02-28 21:29:49 +0100

    Mostrando menús en página de contenido, litado ordenable

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,16 +1,67 @@
 import { useState, useEffect } from "react";
+import { Switch, List, Button, Modal as ModalAntd, notification } from "antd";
+import * as Icon from '@ant-design/icons';
+import Modal from "../../../Modal";
+import DragSortableList from "react-drag-sortable";
+import "./MenuWebList.scss";
+
+const { confirm } = ModalAntd;
 
 export default function MenuWebList(props) {
   const { menu, setReloadMenuWeb } = props;
+  const [listItems, setListItems] = useState([]);
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalContent, setModalContent] = useState(null);
 
-  console.log(menu);
+  useEffect(() => {
+    const listItemsArray = [];
+    menu.forEach(item => {
+      listItemsArray.push({
+        content: (
+          <MenuItem item={item}/>
+        )
+      });
+    });
+    setListItems(listItemsArray);
+  }, [menu]);
+
+  const onSort = (sortedList, dropEvent) => {
+    console.log(sortedList);
+  };
+
   return (
-    <div>
-      <h1>Menu web list</h1>
-      {menu.map(item => (
-        <p key={item._id}>{item.title}</p>
-        ))}
+    <div className="menu-web-list">
+      <div className="menu-web-list__header">
+        <Button type="primary">
+          Menú menú
+        </Button>
+      </div>
+
+      <div className="menu-web-list__items">
+        <DragSortableList items={listItems} onSort={onSort} type="vertical" />
+      </div>
+
     </div>
   )
+}
 
+function MenuItem(props) {
+  const { item, activateMenu, editMenuWebModal, deleteMenu } = props;
+
+  return (
+    <List.Item
+      actions={[
+        <Switch defaultChecked={item.active}/>,
+        <Button type="primary">
+          <Icon.EditOutlined />
+        </Button>,
+        <Button type="danger" onClick={() => deleteMenu(item)}>
+          <Icon.WarningOutlined />
+        </Button>
+      ]}
+    >
+      <List.Item.Meta title={item.title} description={item.url} />
+    </List.Item>
+  );
 }
